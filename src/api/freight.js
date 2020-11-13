3d6871d0f00f5549cbefa229f5a78ef02ccad567
gqy-oray <872647448@qq.com>
2020-11-13 15:09:08 +0800

    运费模板列表

---
+++ src/api/freight.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+import requestJSON from '@/utils/requestJSON'
+// 运费模版列表
+export function getFreightList (data) {
+  return requestJSON({
+    url: '/api/admin/delivery_config/list',
+    method: 'post',
+    data
+  })
+}
+// 运费模版添加/编辑
+export function createFreight (data) {
+  return requestJSON({
+    url: '/api/admin/delivery_config/save',
+    method: 'post',
+    data
+  })
+}
+
+// 运费模版添加/编辑
+export function updateFreight (data) {
+  return requestJSON({
+    url: '/api/admin/delivery_config/save',
+    method: 'post',
+
+    data
+  })
+}
+// 运费模版删除
+export function deleteFreight (data) {
+  return requestJSON({
+    url: '/api/admin/delivery_config/delete',
+    method: 'post',
+    data
+  })
+}
+//获取地区/api/admin/delivery_region/list
+export function getAreaList (data) {
+  return requestJSON({
+    url: '/api/admin/delivery_region/list',
+    method: 'post',
+    data
+  })
+}
