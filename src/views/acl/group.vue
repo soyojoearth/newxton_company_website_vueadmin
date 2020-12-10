6188b1b8e4bfd7e03a0530863b3351e5aa246c65
gqy-oray <872647448@qq.com>
2020-12-10 12:43:26 +0800

    地区组件修改

--- src/views/acl/group.vue
+++ src/views/acl/group.vue
@@ -12,52 +12,38 @@
       </el-row>
     </el-card>
     <el-row>
-      <el-button
-        type="primary"
-        @click="handleCreate"
-      >创建</el-button>
+      <el-button type="primary"
+                 @click="handleCreate">创建</el-button>
     </el-row>
     <el-card style="margin-top:10px">
-      <el-table
-        ref="multipleTable"
-        :data="tableData"
-        tooltip-effect="dark"
-        style="width: 100%;min-height:300px;margin-bottom:20px;"
-      >
+      <el-table ref="multipleTable"
+                :data="tableData"
+                tooltip-effect="dark"
+                style="width: 100%;min-height:300px;margin-bottom:20px;">
         <el-table-column label="权限组名称">
           <template slot-scope="scope">{{ scope.row.groupName }}</template>
         </el-table-column>
-        <el-table-column
-          prop="groupRemark"
-          label="备注"
-        />
+        <el-table-column prop="groupRemark"
+                         label="备注" />
         <el-table-column align="right">
           <template slot-scope="scope">
-            <el-button
-              size="mini"
-              @click="handleEdit(scope.$index, scope.row)"
-            >编辑</el-button>
-            <el-button
-              size="mini"
-              @click="handleDelete(scope.$index, scope.row)"
-            >删除</el-button>
+            <el-button size="mini"
+                       @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button size="mini"
+                       @click="handleDelete(scope.$index, scope.row)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
     </el-card>
 
-    <el-dialog
-      title=""
-      :visible.sync="dialogVisible"
-      width="50%"
-    >
-      <el-form
-        ref="form"
-        label-position="top"
-        :model="form"
-        label-width="80px"
-        status-icon
-      >
+    <el-dialog title=""
+               :visible.sync="dialogVisible"
+               width="50%">
+      <el-form ref="form"
+               label-position="top"
+               :model="form"
+               label-width="80px"
+               status-icon>
         <el-form-item label="权限组名称">
           <el-input v-model="form.groupName" />
         </el-form-item>
@@ -66,32 +52,24 @@
         </el-form-item>
         <el-form-item label="操作权限">
           <template>
-            <el-transfer
-              v-model="aclActionValue"
-              :data="aclActionData"
-              :titles="['全部操作权限', '拥有操作权限']"
-            ></el-transfer>
+            <el-transfer v-model="aclActionValue"
+                         :data="aclActionData"
+                         :titles="['全部操作权限', '拥有操作权限']"></el-transfer>
           </template>
         </el-form-item>
       </el-form>
-      <div
-        slot="footer"
-        class="dialog-footer"
-      >
+      <div slot="footer"
+           class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button
-          type="primary"
-          @click="handPostData()"
-        >确 定</el-button>
+        <el-button type="primary"
+                   @click="handPostData()">确 定</el-button>
       </div>
     </el-dialog>
 
-    <el-dialog
-      title=""
-      :visible.sync="dialogVisibleCreate"
-      width="50%"
-    >
-      <el-form
+    <el-dialog title=""
+               :visible.sync="dialogVisibleCreate"
+               width="50%">
+      <!-- <el-form
         ref="form"
         label-position="top"
         :model="form"
@@ -113,34 +91,25 @@
             ></el-transfer>
           </template>
         </el-form-item>
-      </el-form>
-      <div
-        slot="footer"
-        class="dialog-footer"
-      >
+      </el-form> -->
+
+      <div slot="footer"
+           class="dialog-footer">
         <el-button @click="dialogVisibleCreate = false">取 消</el-button>
-        <el-button
-          type="primary"
-          @click="handPostDataCreate()"
-        >确 定</el-button>
+        <el-button type="primary"
+                   @click="handPostDataCreate()">确 定</el-button>
       </div>
     </el-dialog>
 
-    <el-dialog
-      title="提示"
-      :visible.sync="dialogVisibleConfirmDelete"
-      width="30%"
-    >
+    <el-dialog title="提示"
+               :visible.sync="dialogVisibleConfirmDelete"
+               width="30%">
       <span>确定删除该权限组：{{prepareDeleteGroupName}}</span>
-      <span
-        slot="footer"
-        class="dialog-footer"
-      >
+      <span slot="footer"
+            class="dialog-footer">
         <el-button @click="dialogVisibleConfirmDelete = false">取 消</el-button>
-        <el-button
-          type="primary"
-          @click="handPostDataDelete()"
-        >确 定</el-button>
+        <el-button type="primary"
+                   @click="handPostDataDelete()">确 定</el-button>
       </span>
     </el-dialog>
 
